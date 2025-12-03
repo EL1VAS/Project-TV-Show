4c0dcd9ead5537b1826ee055d9fb95a1aa8f738f
Pythia <el1vas@yahoo.gr>
2025-12-03 19:49:21 +0100

    Added class names to elements for styling

--- script.js
+++ script.js
@@ -13,17 +13,21 @@ function makePageForEpisodes(episodeList) {
     const episode = episodeList[i]; // Each episode in the iteration
 
     const episodeCard = document.createElement("div"); // Main episode card
+    episodeCard.className = "episode-card";
 
     const episodeCode = document.createElement("h2"); // Title of episode
     episodeCode.textContent = `${episode.name} - S${episode.season
       .toString()
       .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`; // Title display with season and episode number padded with a 0 in front
+    episodeCode.className = "episode-title";
 
     const episodeImage = document.createElement("img"); // Image of the episode
     episodeImage.src = episode.image.medium; // Displays the medium image
+    episodeImage.className = "episode-img";
 
     const episodeDescription = document.createElement("p"); // Description of the episode
     episodeDescription.innerHTML = episode.summary; // Displays the summary of the episode without the <p><\p> characters showing
+    episodeDescription.className = "episode-summary";
 
     episodeCard.appendChild(episodeCode); // Appended the title,
     episodeCard.appendChild(episodeImage); // image and
