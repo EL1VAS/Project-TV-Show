5161dafd3acc9cdd1b887a388f7457a402fb8774
Pythia <el1vas@yahoo.gr>
2025-12-03 18:35:37 +0100

    Added the variables and comments

--- script.js
+++ script.js
@@ -5,15 +5,16 @@ function setup() {
   makePageForEpisodes(allEpisodes); // Pass array to the function
 }
 
-function makePageForEpisodes(episodeList) { // Receive it as an episode list
+function makePageForEpisodes(episodeList) {
+  // Receive it as an episode list
   const rootElem = document.getElementById("root"); // Access the root in html
   rootElem.textContent = ""; // Initially keep context to empty
-  fot (let i = 0; i <= episodeList.length; i++) {
+  for (let i = 0; i < episodeList.length; i++) {
     const episode = episodeList[i];
-    document.createElement("div");
-    document.createElement("h2");
-    document.createElement("img");
-    document.createElement("article");
+    const episodeCard = document.createElement("div"); // Main episode caard
+    const episodeTitle = document.createElement("h2"); // Title of episode
+    const episodeImage = document.createElement("img"); // Image of the episode
+    const episodeDescription = document.createElement("article"); // Description of the episode
   }
 }
 
