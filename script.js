3e68dececab8e42cf6b95e4f419ff35b04a79129
Pythia <el1vas@yahoo.gr>
2025-12-03 18:30:04 +0100

    Created the elements for the layout for every episode in the list by iterating through the episode list

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 //You can edit ALL of the code here
+
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  const allEpisodes = getAllEpisodes(); //Get the array
+  makePageForEpisodes(allEpisodes); // Pass array to the function
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+function makePageForEpisodes(episodeList) { // Receive it as an episode list
+  const rootElem = document.getElementById("root"); // Access the root in html
+  rootElem.textContent = ""; // Initially keep context to empty
+  fot (let i = 0; i <= episodeList.length; i++) {
+    const episode = episodeList[i];
+    document.createElement("div");
+    document.createElement("h2");
+    document.createElement("img");
+    document.createElement("article");
+  }
 }
 
 window.onload = setup;
