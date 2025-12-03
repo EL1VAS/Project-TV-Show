d4d5097f9dc858ad29d11203fe6f2ea8e7fa0f88
Pythia <el1vas@yahoo.gr>
2025-12-03 18:56:17 +0100

    Added the context of each element and appended each element to the card and the card to the root element

--- script.js
+++ script.js
@@ -10,11 +10,26 @@ function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root"); // Access the root in html
   rootElem.textContent = ""; // Initially keep context to empty
   for (let i = 0; i < episodeList.length; i++) {
-    const episode = episodeList[i];
-    const episodeCard = document.createElement("div"); // Main episode caard
+    const episode = episodeList[i]; // Each episode in the iteration
+
+    const episodeCard = document.createElement("div"); // Main episode card
+
     const episodeTitle = document.createElement("h2"); // Title of episode
+    episodeTitle.textContent = `${episode.name} - S${episode.season
+      .toString()
+      .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`; // Title display with season and episode number padded with a 0 in front
+
     const episodeImage = document.createElement("img"); // Image of the episode
+    episodeImage.src = episode.image.medium; // Displays the medium image
+
     const episodeDescription = document.createElement("article"); // Description of the episode
+    episodeDescription.textContent = episode.summary; // Displays the summary of the episode
+
+    episodeCard.appendChild(episodeTitle); // Appended the title,
+    episodeCard.appendChild(episodeImage); // image and
+    episodeCard.appendChild(episodeDescription); // summary to the episode card
+
+    rootElem.appendChild(episodeCard); // Appended episode card to root element
   }
 }
 
